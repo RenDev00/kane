d95f3e700bf55f81218c66d8e32e7a71b53f9749
RenDev00 <rene.huber00@gmx.at>
2026-03-05 22:50:30 +0100

    feat: implement field validation for transaction model

--- backend/model/transaction.py
+++ backend/model/transaction.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import StrEnum, auto
 from typing import Optional, Self, Union
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator, AwareDatetime
 
 
 class TransactionType(StrEnum):
@@ -33,11 +33,11 @@ class Transaction(BaseModel):
         examples=[0, 1, 2], description="A unique transaction identifier.", frozen=True
     )
     amount: float = Field(examples=[19.99], description="The value of the transaction.")
-    date: datetime = Field(
+    date: AwareDatetime = Field(
         examples=["2026-03-02T20:50:32Z"], description="The date of the transaction."
     )
     type: TransactionType = Field(
-        examples=["INCOME", "EXPENSE"], description="The type of the transaction."
+        examples=["EXPENSE", "INCOME"], description="The type of the transaction."
     )
     category: TransactionCategory = Field(
         examples=["NEED", "WANT", "SAVING", "SALARY", "OTHER"],
@@ -48,6 +48,47 @@ class Transaction(BaseModel):
         description="An optional transaction comment.",
         default=None,
     )
+
+    @field_validator("date", mode="after")
+    @classmethod
+    def validate_date(cls, v: datetime) -> AwareDatetime:
+        if v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)
+        return v
+
+    @field_validator("type", mode="before")
+    @classmethod
+    def validate_type(cls, v: str | TransactionType) -> TransactionType:
+        op = {
+            str: lambda x: TransactionType[x.upper()],
+            TransactionType: lambda x: x,
+        }
+        try:
+            transaction_type = op[type(v)](v)
+        except KeyError:
+            raise ValueError(f"Invalid transaction type {v}.")
+
+        return transaction_type
+
+    @field_validator("category", mode="before")
+    @classmethod
+    def validate_category(cls, v: str | TransactionCategory) -> TransactionCategory:
+        if isinstance(v, TransactionCategory):
+            return v
+
+        op = {
+            str: lambda x: (
+                TransactionIncomeCategory[x.upper()]
+                if x in TransactionIncomeCategory
+                else TransactionExpenseCategory[x.upper()]
+            ),
+        }
+        try:
+            transaction_category = op[type(v)](v)
+        except KeyError:
+            raise ValueError(f"Invalid transaction category {v}.")
+
+        return transaction_category
 
     @model_validator(mode="after")
     def validate(self) -> Self:
