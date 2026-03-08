f3593b5367d4e2925fd8e9b114fd3e20653d9299
RenDev00 <rene.huber00@gmx.at>
2026-03-08 14:09:43 +0100

    refactor: rework deprecated density parameter

--- frontend/src/pages/Transactions.vue
+++ frontend/src/pages/Transactions.vue
@@ -9,7 +9,7 @@
       </v-col>
     </v-row>
 
-    <v-row class="my-2" dense>
+    <v-row class="my-2" density="comfortable">
       <v-col cols="12">
         <v-text-field
           v-model="filterComment"
@@ -23,7 +23,7 @@
       </v-col>
     </v-row>
 
-    <v-row class="my-2" dense>
+    <v-row class="my-2" density="comfortable">
       <v-col cols="6" sm="3">
         <v-select
           v-model="filterType"
